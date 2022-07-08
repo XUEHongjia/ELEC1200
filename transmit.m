4d319b4bcb50cb85ba7fd571dc7864fca765f87f
XUEHongjia <91336916+XUEHongjia@users.noreply.github.com>
2022-07-08 12:09:18 +0800

    Update transmit.m

--- transmit.m
+++ transmit.m
@@ -1,11 +1,11 @@
-function textwave1=transmit(textmsg)
+function textwave1 = transmit(textmsg)
 SPB=40;
 
-textbit=text2bitseq(textmsg);
-textwave=bitseq2waveform(textbit,SPB);
-training0=zeros(1,200);
-training1=ones(1,200);
-training2=zeros(1,200);
-trainingseq=[training0,training1,training2];
+textbit = text2bitseq(textmsg);
+textwave = bitseq2waveform(textbit,SPB);
+training0 = zeros(1,200);
+training1 = ones(1,200);
+training2 = zeros(1,200);
+trainingseq = [training0,training1,training2];
 
 textwave1 = [ones(1,SPB),trainingseq,textwave];   
