bc7dd225a19aeed606c3ae682c0a4badcf69e444
XUEHongjia <91336916+XUEHongjia@users.noreply.github.com>
2022-07-08 12:23:28 +0800

    Update task2.m

--- task2.m
+++ task2.m
@@ -1,33 +1,47 @@
 SPB = 40;
 
-textbit = text2bitseq(textmsg);
-textwave = bitseq2waveform(textbit,SPB);
+textbit = text2bitseq(textmsg);  % turning text to bits according to ASCII
+textwave = bitseq2waveform(textbit,SPB);  % turning bits to wave
+
+% preparing bits for protocal -> starts
 training0 = zeros(1,200);
 training1 = ones(1,200);
 training2 = zeros(1,200);
 trainingseq = [training0,training1,training2];
+% preparing bits for protocal -> ends
 
-textwave1 = [ones(1,SPB),trainingseq,textwave];   
 
-rx_wave = txrx(textwave1);
+textwave1 = [ones(1,SPB),trainingseq,textwave];  % turning protocal bits to waves  
 
-ind=find_start(rx_wave);    %<=???
+rx_wave = txrx(textwave1); % transmit the protocal through the noisy channel
+
+% sorting out the pramaters for protocal -> starts
+
+ind = find_start(rx_wave);    %<=???
 training_rx0 = rx_wave(1:ind);
 training_rx1 = rx_wave(1:ind+400);
 c = min(training_rx1);
 k = max(training_rx1)-c;
 b = max(training_rx0);
 a = (1-b/k)^(SPB+1);
+
+% sorting out the pramaters for protocal -> ends
+
+% decoding -> starts
+
 decoded_rx = zeros(1,length(rx_wave));
-decoded_rx(1) = rx_wave(1)/[k*(1-a)];
+decoded_rx(1) = rx_wave(1)/[k*(1-a)];  
+
 for m = 2:length(rx_wave)
     decoded_rx(m) = (rx_wave(m)-a*rx_wave(m-1))/(k*(1-a));
 end
+
 for n = 1:ind
     if training_rx0(n) == max(training_rx0)
         start = n
     end
 end
+
 threshold = [max(decoded_rx)+min(decoded_rx)]/2;
 start = start+600
 rxbit = zeros(1,3896);
@@ -41,6 +55,9 @@ rxbit = zeros(1,3896);
      end
 end
 rxtext = bitseq2text(rxbit);
+
+% decoding -> ends
+
 compute_BER(rxbit,rxtext)
 a = decoded_rx(1:5*ind);
 t = 1:5*ind;
